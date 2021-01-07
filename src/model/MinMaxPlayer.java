342219f3eb20bf1fbd76e485ce59d5e4643b0ab5
Axel <axel.deleron@gmail.com>
2021-01-07 23:23:17 +0100

    Implementing MinMaxPlayer class

--- src/model/MinMaxPlayer.java
+++ src/model/MinMaxPlayer.java
@@ -23,10 +23,13 @@ public class MinMaxPlayer extends ComputerPlayer {
 	
 	protected StateSet transpositionTableMax, transpositionTableMin;
 	
+	protected int counter;
+	
 	public MinMaxPlayer(boolean joueur) {
 		this.player = joueur;
 		this.transpositionTableMax = new StateSet();
 		this.transpositionTableMin = new StateSet();
+		this.counter = 0;
 	}
 	@Override
 	public boolean isHuman() {
@@ -40,8 +43,110 @@ public class MinMaxPlayer extends ComputerPlayer {
 		if (game == null || game.isGameOver()) {
 			return;
 		}
+		
+		System.out.println("Update");
+
+		//Getting the next move done by the MinMaxPlayer
+		Move m = minimaxDecision(game);
+		
+		//Applying the move chosen by the MinMaxPlayer
+		game.move(m.getStartIndex(), m.getEndIndex());
+		
+		System.out.println("Chosen move");
+		System.out.println(m);
+		
+		//Creating a new transposition table at each new turn
+		System.out.println(transpositionTableMax.size());
+		System.out.println(transpositionTableMin.size());
+		this.transpositionTableMax = new StateSet();
+		this.transpositionTableMin = new StateSet();
 			
 		//TODO
+	}
+	
+	public int maxValue(Game game) {
+		//We don't go too deep to prevent from stackoverflow
+		counter++;
+		System.out.println(counter);
+		if (counter > 1000) {return game.goodHeuristic(player);}
+		
+		int v = -1000;
+		
+		if(game.isGameOver()) {return game.goodHeuristic(player);}
+		
+		//For each move possible we check what the other player can do recursively
+		for(Move m : getMoves(game))
+		{
+			Game tempGame = game.copy();
+			tempGame.move(m.getStartIndex(), m.getEndIndex());
+			
+			//We check if we already encountered this state
+			if(transpositionTableMax.getValue(tempGame) != null){return v;}
+			
+			//We check if the same player can play multiple times because of skips
+			if(!tempGame.isP2Turn())
+			{
+				v = Math.min(v, maxValue(tempGame)); //If skips are available
+			}
+			else 
+			{
+				v = Math.max(v, minValue(tempGame)); //If skips aren't available
+			}
+			
+		}
+		transpositionTableMax.add(game, v);
+		return v;
+	}
+	
+	public int minValue(Game game) {
+		counter++;
+		System.out.println(counter);
+		if (counter > 1000) {return game.goodHeuristic(player);}
+		int v = 1000;
+		if(game.isGameOver()) {return game.goodHeuristic(player);}
+		for(Move m : getMoves(game))
+		{
+			Game tempGame = game.copy();
+			tempGame.move(m.getStartIndex(), m.getEndIndex());
+			
+			//We check if we already encountered this state
+			if(transpositionTableMin.getValue(tempGame) != null){return v;}
+			
+			if(tempGame.isP2Turn())
+			{
+				v = Math.max(v, minValue(tempGame));
+			}
+			else
+			{
+				v = Math.min(v, maxValue(tempGame));
+			}
+			
+		}
+		transpositionTableMin.add(game, v);
+		return v;
+	}
+	
+	public Move minimaxDecision(Game game) {
+		//We initiate the first value to which we'll compare the rest
+		Game tempGame = game.copy();
+		Move m = getMoves(game).get(0);
+		tempGame.move(m.getStartIndex(), m.getEndIndex());
+		int v = minValue(tempGame);
+		
+		//We check each values from the possible moves and choose the best one for the MIN player
+		for(Move mTemp :getMoves(game))
+		{
+			counter = 0;
+			tempGame = game.copy();
+			tempGame.move(mTemp.getStartIndex(), mTemp.getEndIndex());
+			int vTemp = minValue(tempGame);
+			if(vTemp > v)
+			{
+				m = mTemp;
+				v = vTemp;
+			}
+		}
+		return m;
 	}
 	
 	/**
